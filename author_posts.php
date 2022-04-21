c03d85ce8280edac4361f081be9df7994b2b9f2a
delphinmico <delphinmicomyiza@gmail.com>
2022-04-21 22:37:57 +0200

    done

--- author_posts.php
+++ author_posts.php
@@ -50,8 +50,7 @@ $select_all_posts_query = mysqli_query($connection,$query);
                 
                 <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
 
-                the image
-                
+
                 <hr>
                 <p><?php echo $post_content; ?></p>
                 
