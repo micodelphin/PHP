49c0c56e609cbd44b1707e129e139fa560d01da5
mico delphin <delphinmicomyiza@gmail.com>
2022-07-18 16:24:37 +0200

    initial commit

---
+++ variables.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+<meta charset="UTF-8">
+<title>Document</title>
+    
+</head>
+<body>
+    
+   <?php
+  
+   $name = 'delphin';
+   $number = 100;
+
+   echo $name;
+
+   ?>
+    
+</body>
+</html>
