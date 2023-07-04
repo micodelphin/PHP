636e1d544128616a6d6316ae67019b1b796267ba
mico delphin <delphinmicomyiza@gmail.com>
2023-07-04 21:52:35 +0200

    i edit and put more here

--- index.php
+++ index.php
@@ -87,7 +87,7 @@
 
             <hr>
             <p><?php echo $post_content; ?></p>
-            <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More About me <span
+            <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">More Here <span
                     class="glyphicon glyphicon-chevron-right"></span></a>
 
             <hr>
